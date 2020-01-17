e4dee0976c20b28b7cee1f716c28c081ee04006f
jerrykim91 <sun4131@gmail.com>
2020-01-17 17:59:37 +0900

    GDP_project_2020.01.17(Create history : base setting_1)

--- member/models.py
+++ member/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # 멤버 =>  auth에서  
@@ -11,4 +8,13 @@ class LIST(AbstractUser):
     
 
 # 정리 
-# 
+# 1. memeber app 설치
+# 2. urls.py 생성
+# 3. setting.py에 AUTH_USER_MODEL="member.LIST"("app이름.Class이름")
+# 4. models.py
+#     from django.contrib.auth.models import AbstractUser
+#     class LIST(AbstractUser):
+#         userid = models.CharField(max_length=200, null=True)
+#         birth = models.IntegerField(null=True)
+# 5. 1:1 모델생성 
+    
